5625a9f53cadc55a6f2273b3b3ba4abee1e00935
Edward Huang <edwardgunawan880@yahoo.com>
2019-08-18 18:08:01 -0400

    tryna fix this tag cloud thing, hopefully it works this time

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "edwardHuang-20190818175449";
+var CACHE_NAME = "edwardHuang-20190818180456";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?201908181754",
+        "/css/style.css?201908181804",
         "/"
       ]);
     })
